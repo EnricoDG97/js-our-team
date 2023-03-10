06354b5d72f5d9a32ffee9b0cc51bba25d44c4d1
EnricoDG97 <enrico.delgaudio97@gmail.com>
2023-03-10 06:55:37 +0100

    feat: added milestone 2 +bonus

--- script.js
+++ script.js
@@ -18,33 +18,64 @@ let members = [
     {
         name: "Wayne Barnett",
         role: "Founder & CEO",
-        image: "wayne-barnett-founder-ceo.jpg",
+        image: "wayne-barnett-founder-ceo.jpg"
     },
     {
         name: "Angela Caroll",
         role: "Chief Editor",
-        image: "angela-caroll-chief-editor.jpg",
+        image: "angela-caroll-chief-editor.jpg"
     },
     {
         name: "Walter Gordon",
         role: "Office Manager",
-        image: "walter-gordon-office-manager.jpg",
+        image: "walter-gordon-office-manager.jpg"
     },
     {
         name: "Angela Lopez",
         role: "Social Media Manager",
-        image: "angela-lopez-social-media-manager.jpg",
+        image: "angela-lopez-social-media-manager.jpg"
     },
     {
         name: "Scott Estrada",
         role: "Developer",
-        image: "scott-estrada-developer.jpg",
+        image: "scott-estrada-developer.jpg"
     },
     {
         name: "Barbara Ramos",
         role: "Graphic Designer",
-        image: "barbara-ramos-graphic-designer.jpg",
+        image: "barbara-ramos-graphic-designer.jpg"
     },
 ];
 
-// MILESTONE 2: Stampo tutte le informazioni sottoforma di stringa
+console.log(members);
+
+// MILESTONE 2 + bonus
+
+const membersContainerEl = document.getElementById("membersContainer");
+
+
+for (let i=0; i < members.length; i++) {
+    let newMember = document.createElement("div");
+
+    membersContainerEl.append(newMember);
+    newMember.classList.add("card");
+
+    let currentMember = members[i];
+
+    // creo elementi per ciascuna chiave
+    let memberImage = document.createElement("img");
+    let memberName = document.createElement("div");
+    let memberRole = document.createElement("div");
+    // aggiungo stilizzazione con classi
+    memberImage.classList.add("image");
+    memberName.classList.add("title");
+    memberRole.classList.add("role");
+    // stampo in pagina
+    memberImage.src = "img/" + currentMember.image;
+    memberName = currentMember.name;
+    memberRole = currentMember.role;
+    // appendo gli elementi
+    newMember.append(memberImage);
+    newMember.append(memberName);
+    newMember.append(memberRole);
+}
